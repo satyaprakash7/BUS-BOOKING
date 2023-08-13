80bd8a9b430a7b47dc8d6a90327144bb902f5b21
Satyaprakash7 <satyaprakash.sahoo840@gmail.com>
2023-08-13 15:59:52 +0530

    enter

--- server.js
+++ server.js
@@ -12,14 +12,16 @@ const bookingsRoute = require("./routes/bookingsRoute");
 app.use("/api/users", usersRoute);
 app.use("/api/buses", busesRoute);
 app.use("/api/bookings", bookingsRoute);
+
+// deployment config
 const path = require("path");
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Node server listening on port ${port}!`));
